4f5bf15a5b8a415502ca1ab55acc1ccdd77f00af
KienTran <kien.tran.oris@gmail.com>
2021-02-24 17:56:43 +0700

    v3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,8 +28,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "46634449315f0105e815efe507637c8d",
-"/": "46634449315f0105e815efe507637c8d",
+"index.html": "0a2d7352d120cbce551507bb3da14d56",
+"/": "0a2d7352d120cbce551507bb3da14d56",
 "main.dart.js": "03318f0c98062f81b04cb2b087cd8586",
 "manifest.json": "56ef5909c85760fa8533c46b8023d4aa",
 "version.json": "718847d3fd4a2a41aa33101c1fb12a28"
